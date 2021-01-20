24839261f3ccc0d7f9f51c48a2fc7c560928fddc
Igor Silaev <cokturqi55@mail.ru>
2021-01-20 21:50:48 +0300

    Blaming regex.

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
-//kek
-
 void main() {
   runApp(
     FriendlyChatApp(),
