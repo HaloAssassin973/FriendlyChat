df5f77eb4e83790bbf14cbf18becda079bf858ac
Igor Silaev <cokturqi55@mail.ru>
2021-01-20 21:48:31 +0300

    Don't Ask Me, I Have No Idea Why This Works Either

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
+//kek
+
 void main() {
   runApp(
     FriendlyChatApp(),
